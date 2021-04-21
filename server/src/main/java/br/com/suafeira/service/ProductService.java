38d6a759666ffffc5f7625973c3f851986f942c6
Marcos Marossi <marcos.marossi@fatec.sp.gov.br>
2021-04-21 09:46:51 -0300

    Adiciona Bean a Product Service

--- server/src/main/java/br/com/suafeira/service/ProductService.java
+++ server/src/main/java/br/com/suafeira/service/ProductService.java
@@ -3,10 +3,12 @@ package br.com.suafeira.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import br.com.suafeira.repository.ProductRepository;
 import br.com.suafeira.to.ProductTO;
 
+@Service
 public class ProductService {
 	
 	@Autowired
