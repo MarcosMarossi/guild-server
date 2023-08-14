7a4a3f3470af6a0b0ad61cb78173caf3dfd2a47d
MarcosMarossi <marcos.marossi@fatec.sp.gov.br>
2023-08-14 17:44:11 -0300

    Remove spring cache

--- GuildAPI/src/main/java/br/com/feira/guild/Main.java
+++ GuildAPI/src/main/java/br/com/feira/guild/Main.java
@@ -2,12 +2,10 @@ package br.com.feira.guild;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @EnableSpringDataWebSupport
 @SpringBootApplication
-@EnableCaching
 public class Main {
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
