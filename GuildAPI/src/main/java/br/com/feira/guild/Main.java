56da5596f5fb7e88a50940ab5df30869452ab60f
MarcosMarossi <marcos.marossi@fatec.gov.sp.br>
2023-10-04 07:43:13 -0300

    Registering microservices

--- GuildAPI/src/main/java/br/com/feira/guild/Main.java
+++ GuildAPI/src/main/java/br/com/feira/guild/Main.java
@@ -2,10 +2,12 @@ package br.com.feira.guild;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @EnableSpringDataWebSupport
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Main {
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
